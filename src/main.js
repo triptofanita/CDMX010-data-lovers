f94daf5267ac754f255a725ca4039c0be23bd0e9
Talia Gonzalez <taliagonzalez.dla@gmail.com>
2021-02-22 21:23:58 -0600

    Add features for js

--- src/main.js
+++ src/main.js
@@ -1,158 +1,125 @@
+///Importaciones
+import {orderData, filterData, searchPokemon, getPokemones} from './data.js';
+import { btnAllData, containerAllData, order, types, btnSearch} from './variables.js';
 
-// import { example } from './data.js';
-// // import data from './data/lol/lol.js';
-//  import data from './data/pokemon/pokemon.json';
-// // import data from './data/rickandmorty/rickandmorty.js';
-const resultado = document.getElementById('table-poke');
-const url = './data/pokemon/pokemon.json';
-<<<<<<< HEAD
-//Ordenar lista de nombre A - Z
-const orderAz = (listaPokex) => {
-    listaPokex.sort(function compare(a, b) {
-    if  (a.name < b.name
-    ) return -1;
-    if (a.name > b.name
-    ) return 1;
-      return 0;
-    })
-    console.log(listaPokex)
-};
+//Eventos
+btnAllData.addEventListener("click", getData)
+btnSearch.addEventListener("click", searchData)
 
-//Ordenar lista de nombres Z - A
-const orderZa = (listaPokex_) => {
-    listaPokex_.sort(function compare(b, a) {
-    if (a.name < b.name
-    ) return 1;
-    if (a.name > b.name
-    ) return -1;
-      return 0;
-    })
-    console.log(listaPokex_)
-};
-
-//Método Fetch
-fetch(url)
-    .then(response => response.json())
-    .then(data => {
-        // debugger
-        // renderPokemon(data.img)
+//Funcion principal
+function getData() {
+    getPokemones().then((data) => {
         let pokemones = data.pokemon;
-        renderData(pokemones);
-    })
-
-=======
-let resultOrderAZ = document.getElementById('resultadosAZ');
-let resultOrderZA = document.getElementById('resultadosZA');
-
-// Funcion Leer Datos
->>>>>>> a03a87c631c23d25d8311829e9c3acbe73ab5311
-const renderData = (pokemones) => {
-    pokemones.forEach((index) => {
-        let dataPokemones = `
-       <td><img  src="${index.img}" height="100"/></td>
-       <td>${index.num}</td>
-       <td>${index.name}</td>
-       <td>${index.generation.name}</td>
-       <td class="fila1" id="details-r1">
-            <a class="img"><img id="detail" src="img/pokebola.png"/></a>
-        </td>
-        `;
-        resultado.insertAdjacentHTML("beforeend", dataPokemones);
+        document.getElementById("containerData").innerHTML="";
+        pokemones.forEach((index) => {
+            containerAllData.innerHTML += `
+                <div class="card">
+                    <div class="img-container">
+                        <img src="${index.img}">
+                    </div>
+                <div class="info">
+                    <span class= "number"> ${index.num}</span> 
+                    <h3>${index.name}</h3>
+                    <small class="type">Type: <span>${index.type}</span></small>
+                </div>
+                <div class="weight">
+                    <small class="type">Weight: <span>${index.size.weight}</span></small>
+                </div>
+                <div class="height">
+                    <small class="type">Height: <span>${index.size.height}</span></small>
+                </div>
+                </div>
+            `
+        });
     });
 }
 
-//Funcion Orden A-Z
-const orderDataAZ = (pokemonesData) => {
-    pokemonesData.sort(function compare(a,b) {
-        if (a.name > b.name)
-            return 1;
-        if (a.name < b.name)
-            return -1;
-        return 0;
+// Evento orderData
+getPokemones().then((data) => {
+    let pokemones = data.pokemon;
+    order.addEventListener('change', (e) => {
+        let orderPoke = orderData(pokemones,e.target.value);
+        // console.log(orderPoke);
+        document.getElementById("containerData").innerHTML="";
+        orderPoke.forEach((index) => {
+            containerAllData.innerHTML += `
+            <div class="card">
+            <div class="img-container">
+                <img src="${index.img}">
+            </div>
+            <div class="info">
+                <span class= "number"> ${index.num}</span> 
+                <h3>${index.name}</h3>
+                <small class="type">Type: <span>${index.type}</span></small>
+            </div>
+            <div class="weight">
+                <small class="type">Weight: <span>${index.size.weight}</span></small>
+            </div>
+            <div class="height">
+                <small class="type">Height: <span>${index.size.height}</span></small>
+            </div>
+            </div>
+            `;
+        })
     })
-    //Operador Terniarios
-    // pokemonesData.sort((a, b) => (a.name > b.name) ? 1 : (b.name > a.name) ? -1 : 0);
-    pokemonesData.forEach((position) => {
-        let dataOrder  = `
-        <td><img  src="${position.img}" height="100"/></td>
-        <td>${position.num}</td>
-        <td>${position.name}</td>
-        <td>${position.generation.name}</td>
-        <td class="fila1" id="details-r1">
-             <a class="img"><img id="detail" src="img/pokebola.png"/></a>
-         </td>
-         `;
-         resultOrderAZ.insertAdjacentHTML("beforeend", dataOrder);
-    })
-}
+});
 
-//Funcion Ordenamiento Z-A
-const orderDataZA = (pokemonesData) => {
-    pokemonesData.sort(function compare(a,b) {
-        if (a.name > b.name)
-            return -1;
-        if (a.name < b.name)
-            return 1;
-        return 0;
+//Evento filtro
+getPokemones().then((data) => {
+    let pokemones = data.pokemon;
+    types.addEventListener('change', (e) => {
+        let typePoke = filterData(pokemones,e.target.value);
+        // console.log(typePoke);
+        document.getElementById("containerData").innerHTML="";
+        typePoke.forEach((index) => {
+            containerAllData.innerHTML += `
+            <div class="card">
+            <div class="img-container">
+                <img src="${index.img}">
+            </div>
+            <div class="info">
+                <span class= "number"> ${index.num}</span> 
+                <h3>${index.name}</h3>
+                <small class="type">Type: <span>${index.type}</span></small>
+            </div>
+            <div class="weight">
+                <small class="type">Weight: <span>${index.size.weight}</span></small>
+            </div>
+            <div class="height">
+                <small class="type">Height: <span>${index.size.height}</span></small>
+            </div>
+            </div>
+            `;
+        })
     })
-    // Operador Terniario
-    // pokemonesData.sort((a, b) => (a.name > b.name) ? -1 : (b.name > a.name) ? 1 : 0);
-    pokemonesData.forEach((position) => {
-        let dataOrder  = `
-        <td><img  src="${position.img}" height="100"/></td>
-        <td>${position.num}</td>
-        <td>${position.name}</td>
-        <td>${position.generation.name}</td>
-        <td class="fila1" id="details-r1">
-             <a class="img"><img id="detail" src="img/pokebola.png"/></a>
-         </td>
-         `;
-         resultOrderZA.insertAdjacentHTML("beforeend", dataOrder);
-    })
-}
+});
 
-// Fetch Leer Data
-fetch(url)
-    .then(response => response.json())
-    .then(data => {
-        // debugger
-        // renderPokemon(data.img)
+//Evento Buscar por nombre
+function searchData() {
+    getPokemones().then((data) => {
         let pokemones = data.pokemon;
-        renderData(pokemones);
-    })
-
-// Fetch Orden A-Z
-    fetch (url)
-    .then((response)=> response.json ())
-    .then((data) => {
-    // debugger
-    let pokedex = data.pokemon;
-    orderDataAZ(pokedex);
-});
-
-//Fetch Orden Z-A
-fetch (url)
-.then((response)=> response.json ())
-.then((data) => {
-// debugger
-let dataInversa = data.pokemon;
-orderDataZA(dataInversa);
-});
-
-<<<<<<< HEAD
-let pokedexList = fetch (url)
-    .then((response)=> response.json ())
-    .then((data) => {
-    let pokedexList = data.pokemon;
-    pokedexList.sort(function compare (b, a) {
-   if (b.name < a.name
-   ) return 1;
-   if (b.name > a.name
-   ) return -1;
-   return 0;
-});
-    console.log(pokedexList)
-});
-
-=======
->>>>>>> a03a87c631c23d25d8311829e9c3acbe73ab5311
+        let textName = (document.getElementById("namePoke").value).toLowerCase();
+        let pokemonSearch = searchPokemon(pokemones,textName);
+        document.getElementById("containerData").innerHTML="";
+        pokemonSearch.forEach((index) => {
+            containerAllData.innerHTML += `
+        <div class="card" id="showSearch">
+        <div class="img-container">
+            <img src="${index.img}">
+        </div>
+        <div class="info">
+            <span class= "number"> ${index.num}</span> 
+            <h3>${index.name}</h3>
+            <small class="type">Type: <span>${index.type}</span></small>
+        </div>
+        <div class="weight">
+            <small class="type">Weight: <span>${index.size.weight}</span></small>
+        </div>
+        <div class="height">
+            <small class="type">Height: <span>${index.size.height}</span></small>
+        </div>
+        </div>
+        `;
+        })
+    });
+}
--- test/data.spec.js
+++ test/data.spec.js
@@ -1,23 +1,39 @@
-import { example, anotherExample } from '../src/data.js';
+import { getPokemones, filterData } from '../src/data.js';
 
+describe('Probar Async/Await', () => {
 
-describe('example', () => {
-  it('is a function', () => {
-    expect(typeof example).toBe('function');
+  it('Realizar peticion a una api', async () => {
+
+    await getPokemones().then(data => {
+
+      expect(data.results).toBeGreaterThan[0];
+
+    });
   });
+});
 
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
-  });
+
+describe('Probar filtrar pokemones', () => {
+  it('Filtrar pokemones por tipo', async () => {
+    await getPokemones().then(data => {
+      expect(filterData(data.pokemon, "poison")[0].type).toContain( "poison");
+     })
+  })
+});     
+      
+
+/*it('returns `example`', () => {
+   expect(example()).toBe('example');
+ });
 });
 
 
 describe('anotherExample', () => {
-  it('is a function', () => {
-    expect(typeof anotherExample).toBe('function');
-  });
+ it('is a function', () => {
+   expect(typeof anotherExample).toBe('function');
+ });
 
-  it('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
-  });
-});
+ it('returns `anotherExample`', () => {
+   expect(anotherExample()).toBe('OMG');
+ });
+});*/
